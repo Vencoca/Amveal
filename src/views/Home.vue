82b06fd7ec59fab4f9bad23f5afcde762d5ab1eb
Vencoca <vaclav.kesler@gmail.com>
2021-07-17 15:39:32 +0200

    Header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero">
+      <div class="hero-text container">
+        <h4>Hero section</h4>
+        <hr />
+        <h2>
+          Lorem ipsum dolor sit amet, consectetur 
+          adipiscing elit.
+        </h2>
+        <hr />
+      </div>
+    </section>
+    <section><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et cum, velit minima voluptatum veniam beatae tempora sint modi molestiae, ex quasi vel voluptas saepe eos? Accusantium doloremque unde nesciunt suscipit voluptas, saepe sequi possimus quasi modi obcaecati nulla eaque, autem sit, tempora harum architecto perspiciatis dolor ipsum? Aliquam veritatis quidem iusto mollitia est nobis dicta, incidunt iste impedit velit! Atque alias animi consequatur mollitia itaque, eveniet laudantium et, distinctio velit nihil repellat aut fugit optio quae ipsum impedit? Quisquam totam ad repellendus asperiores pariatur nostrum qui repudiandae eveniet quam, eos culpa velit corrupti doloribus excepturi dolor accusantium architecto explicabo perspiciatis, enim debitis sapiente modi. Consequatur quidem unde, illo explicabo ratione minima expedita officiis inventore rerum accusantium totam eligendi eaque cum magnam reiciendis distinctio fugiat autem ipsa temporibus minus sapiente, cupiditate nisi itaque? Debitis dicta eum molestias modi ratione officiis inventore expedita nisi dolore ex sequi obcaecati blanditiis, soluta ipsum pariatur ab nihil quam beatae incidunt qui voluptatem nobis. Fugit, recusandae natus illum in sit dolor nihil hic perferendis iure rerum atque? Sapiente, aliquid repellendus. Ipsum, optio perferendis eveniet hic similique tempore iure impedit fugit itaque tempora doloremque quos consequatur at reiciendis quam molestias ratione. Provident impedit nulla dolorum doloremque placeat obcaecati minus! Tempora rem nam optio repudiandae cumque temporibus fugit quibusdam, eligendi perspiciatis veniam laudantium quo laborum dolorem praesentium architecto quaerat amet, labore odit sequi, impedit nemo iusto suscipit voluptatibus ducimus! Corporis consectetur dolorem fuga, odit explicabo facere sint nulla corrupti sapiente cum? Voluptatem corrupti quam enim vel eius corporis nostrum architecto, reprehenderit, ipsa quidem adipisci, non assumenda nobis iure facilis vero? Soluta libero amet pariatur ipsam dicta assumenda voluptatem nam? Eligendi asperiores non sapiente enim? Labore soluta adipisci officia vero iure! Dolore omnis ex commodi culpa odio dolores error autem labore eos sequi eveniet exercitationem suscipit repellat quae eius, fugiat ipsum soluta iste! Ullam sapiente quidem sunt quae minima, quos repellat ipsum saepe qui aut iure nihil nostrum ex aliquid. Dolore, sit unde vero eveniet voluptatum, velit architecto, dolorum libero expedita vel accusamus. Tenetur ipsam non iste minus inventore facere reiciendis ex ad, impedit dignissimos voluptate incidunt quae praesentium tempora quidem nostrum! Fugit iure iste sequi tenetur sed blanditiis, et laudantium alias ullam vel quae unde quis maxime eum eius odio repudiandae esse architecto necessitatibus. Magni saepe earum minima nihil voluptate tenetur molestias dignissimos culpa esse inventore? Sit itaque praesentium explicabo consectetur optio eveniet sunt veniam quod, facere quos similique impedit vero minus, consequatur assumenda, corrupti dolorum reprehenderit quis molestias? Dolorum praesentium veniam ab laboriosam atque eligendi sint porro velit et. Quibusdam fugit ab aperiam molestias officia nisi delectus debitis commodi optio, architecto corporis sequi, maxime impedit tempora distinctio. Molestias accusamus magnam at provident aliquid. Architecto quidem eum quis quisquam tempore assumenda illo unde minus quasi est, aspernatur debitis ipsum saepe reprehenderit reiciendis velit beatae temporibus accusantium deserunt dolore obcaecati culpa nam ea? Reprehenderit officia vel ratione facere laudantium impedit quisquam aliquid tenetur, harum officiis! Ullam, odio dignissimos corrupti, velit possimus officiis, reprehenderit architecto laudantium tempora ipsam eos quisquam deserunt voluptates vitae animi. Adipisci illum voluptatem quas magnam ab? Consequuntur repudiandae labore necessitatibus dolorum! Aperiam quia ipsum libero perferendis odio dicta? At voluptatibus sequi similique laborum quod et suscipit, perferendis corrupti unde blanditiis possimus eveniet nobis alias harum voluptas fugiat accusantium laudantium cupiditate asperiores quos officiis quidem dolorum? Ullam qui maiores cum quaerat nemo molestias, quidem sunt, commodi neque modi atque dignissimos deleniti laudantium dolorum asperiores, tempore consequuntur ipsam deserunt illum culpa rem reiciendis officiis? Iure, enim? Provident mollitia sequi eaque aliquid amet voluptatem sed nisi nemo, vel id libero ipsam excepturi impedit dolore in officiis distinctio magni labore delectus quod saepe temporibus iusto. A quae illum, velit dignissimos ducimus nesciunt eum! A minus, placeat nostrum labore, neque sit dolor, porro repellendus consequatur harum vitae voluptate et exercitationem eaque inventore maxime. Fuga alias nostrum asperiores eaque harum eius eum unde a porro ratione excepturi pariatur ex rerum optio, distinctio tenetur rem perferendis debitis quos architecto facere nobis! Voluptatum doloribus suscipit dicta accusantium provident voluptates magnam temporibus ipsam distinctio reiciendis atque numquam asperiores ea dolorem nesciunt obcaecati accusamus tempore, quidem corporis optio at quos! Enim, rerum eos repudiandae fuga doloribus, explicabo, cumque a ipsam omnis delectus magni ratione laborum. Molestiae minus quae libero, labore necessitatibus officia unde eum laborum nihil facere fugiat modi quis voluptatum voluptates vel dignissimos soluta nisi praesentium rerum expedita illum pariatur aut! Dolor temporibus id eos atque quasi a saepe, facere impedit perferendis sint, velit tempora nulla perspiciatis quidem numquam dolorum. Ipsum quaerat necessitatibus impedit consectetur numquam quas eveniet magni, odio facilis, fuga hic incidunt voluptas accusantium non saepe quasi ipsa. Voluptatibus, ut? Accusamus cum quaerat excepturi quo dolorem debitis nemo quia sint eveniet sapiente! Ipsum dolorum magni tempore fuga, nisi vitae rem, officia tenetur, autem maxime non consequuntur. Odit omnis modi neque minima dignissimos quod! Voluptates, consequuntur culpa eos accusamus omnis beatae qui rerum natus, illum explicabo amet. Inventore quidem quo, beatae, excepturi consequatur porro velit, quis accusamus dicta reprehenderit esse fugit nam distinctio culpa corrupti vero in ea voluptatum! Ipsum sapiente amet hic laboriosam maxime, repudiandae ut! Itaque necessitatibus voluptate nisi! Dolore praesentium repellendus laborum, molestias neque molestiae perferendis cum ratione ipsa repudiandae! Explicabo nisi quisquam nemo perspiciatis facilis, vitae ex cumque, possimus inventore cum excepturi saepe dolores molestias architecto numquam eius expedita ut obcaecati non unde. Provident eveniet laudantium at, eaque quae repellat dicta deserunt iusto. Officia fugiat facere error id necessitatibus fugit. Temporibus quam doloremque veniam soluta! Voluptates delectus minus laudantium, beatae sapiente ex accusamus dolore dolor, magni omnis voluptatibus ab inventore quaerat praesentium reiciendis. Ratione quaerat adipisci, nobis repellat tenetur inventore vitae sit soluta quae totam. Ab sunt magni officia consectetur odit tenetur minima sequi ad dolorem harum. Atque maxime enim animi odio quas magni nisi, ducimus nemo, labore fuga debitis sit, sed quia consectetur porro impedit illo blanditiis illum explicabo. Aut adipisci nulla autem, iste molestias neque ratione praesentium. Culpa nihil earum repellendus similique explicabo ratione, illum tempore voluptatibus, odio dolorem, voluptatem delectus inventore aut ipsam. Odio, praesentium sed!</p></section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  background-image: url("../assets/hero-bg.png");
+  background-attachment: fixed;
+  position: relative;
+  height: 100vh;
+
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+  .hero-text {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #fff;
+
+    h4 {
+      text-transform: uppercase;
+      font-size: 22px;
+      padding-bottom: 4px;
+    }
+
+    h2 {
+      font-size: 50px;
+      @media (min-width: 550px) {
+        font-size: 80px;
+      }
+    }
+
+    hr:nth-child(2) {
+      max-width: 365px;
+      margin-bottom: 16px;
+    }
+    hr:nth-child(4) {
+      height: 6px;
+      background-color: #fff;
+      border: none;
+      max-width: 85px;
+      margin-top: 16px;
+    }
   }
 }
-</script>
+</style>
